51bf938950dd3485a3922a306cc64dc8635728a9
Ben Patterson <ben@robotjamie.com>
2011-10-20 17:56:59 +1100

    Initial Commit. New Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MultiQueue::Application.config.secret_token = '2ba1564432bb096b1e56ccea65c8743299f6b6a539065a8e1ff8221af86be3b982192103e8666be751989258fd1515bc07a3e07cf2cbf070f55631b2d8415c72'
